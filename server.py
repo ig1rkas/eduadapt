5a41c0bcf9cfea34a10a5047db81e0622e95c00c
LedviL <samrinis@mail.ru>
2025-11-22 11:42:07 +0300

    added test generator

--- server.py
+++ server.py
@@ -386,6 +386,41 @@ def deepseekApi(user_prompt: str, system_prompt="You are helpful assistant") -> 
     else:
         return {"status": response.status_code, "reason": response.text}
 
+def get_summarising_test(source_text: str):
+    prompt = f"""
+ЗАДАЧА:
+Составь на основе приведенного ниже текста тест из нескольких вопросов, чтобы проверить, как читатель понял его содержание. Для каждого вопроса сделай несколько вариантов ответа, из которых верным будет только один. Количество вопросов и вариантов ответов определи сам, исходя из длины текста и количества важной информации в нем. При генерации ответа не используй разметку, отправь чистый текст, как указано в шаблоне ниже.
+
+ИСХОДНЫЙ ТЕКСТ: 
+{source_text}
+
+ФОРМАТ ОТВЕТА (JSON):
+{{
+"success": true,
+"data": {{
+    "questions": [
+      {{
+        "id": <порядковый номер вопроса, начиная с 1>,
+        "question": "<вопрос>",
+        "type": "one_choice",
+        "options": [
+          "<вариант 1>",
+          "<вариант 2>", 
+          …
+        ],
+        "correct_answer": <номер правильного ответа>,
+        "explanation": "<цитата из текста, по которой можно определить, что данный ответ является правильным>"
+      }}
+    ],
+    "test_config": {{
+      "total_questions": <количество вопросов>
+    }}
+  }},
+  "error": null
+}}
+"""
+    return json.loads(deepseekApi(prompt)["data"])
+
 
 METRICS = [
     "level_number",
