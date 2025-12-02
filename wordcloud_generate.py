7c7375342ed0bfdef2f86f4492a7fa84928a6a87
sonyamogilny-max <sonyamogilny@gmail.com>
2025-12-02 20:33:09 +0300

    fix wordcloud

--- wordcloud_generate.py
+++ wordcloud_generate.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
 import re
 import base64
+import nltk
 
 from typing import Dict, Any
 from wordcloud import WordCloud
 from io import BytesIO
+from pymorphy3 import MorphAnalyzer
+from nltk.corpus import stopwords
+
 
 
 class WordCloudGenerator:
@@ -15,6 +19,7 @@ class WordCloudGenerator:
         self.height = height
         self.background_color = background_color
         self.max_words = max_words
+
 
     def preprocess_text(self, text: str) -> str:
         """Предобработка текста"""
@@ -27,36 +32,25 @@ class WordCloudGenerator:
         # Удаление лишних пробелов
         text = re.sub(r'\s+', ' ', text).strip()
 
-        return text
+        morph = MorphAnalyzer()
+        lemms = [morph.parse(i)[0].normal_form for i in text]
+
+        return lemms
 
     def remove_stopwords(self, text: str, custom_stopwords: list = None) -> str:
         """Удаление стоп-слов"""
-        # Базовые русские стоп-слова
-        russian_stopwords = {
-            'и', 'в', 'во', 'не', 'что', 'он', 'на', 'я', 'с', 'со', 'как', 'а',
-            'то', 'все', 'она', 'так', 'его', 'но', 'да', 'ты', 'к', 'у', 'же',
-            'вы', 'за', 'бы', 'по', 'только', 'ее', 'мне', 'было', 'вот', 'от',
-            'меня', 'еще', 'нет', 'о', 'из', 'ему', 'теперь', 'когда', 'даже',
-            'ну', 'вдруг', 'ли', 'если', 'уже', 'или', 'ни', 'быть', 'был',
-            'него', 'до', 'вас', 'нибудь', 'опять', 'уж', 'вам', 'ведь', 'там',
-            'потом', 'себя', 'ничего', 'ей', 'может', 'они', 'тут', 'где', 'есть',
-            'надо', 'ней', 'для', 'мы', 'тебя', 'их', 'чем', 'была', 'сам', 'чтоб',
-            'без', 'будто', 'чего', 'раз', 'тоже', 'себе', 'под', 'будет', 'ж',
-            'тогда', 'кто', 'этот', 'того', 'потому', 'этого', 'какой', 'совсем',
-            'ним', 'здесь', 'этом', 'один', 'почти', 'мой', 'тем', 'чтобы', 'нее',
-            'сейчас', 'были', 'куда', 'зачем', 'всех', 'никогда', 'можно', 'при',
-            'наконец', 'два', 'об', 'другой', 'хоть', 'после', 'над', 'больше',
-            'тот', 'через', 'эти', 'нас', 'про', 'всего', 'них', 'какая', 'много',
-            'разве', 'три', 'эту', 'моя', 'впрочем', 'хорошо', 'свою', 'этой',
-            'перед', 'иногда', 'лучше', 'чуть', 'том', 'нельзя', 'такой', 'им',
-            'более', 'всегда', 'конечно', 'всю', 'между'
-        }
+
+        nltk.download('stopwords')
+        stopwords_en = set(stopwords.words('english'))
+        stopwords_ru = set(stopwords.words('russian'))
+        additional_words = ['очень', 'которые', 'это', 'ещё', 'также', 'который', 'например']
+        combined_stopwords = stopwords_en.union(stopwords_ru).union(additional_words)
 
         if custom_stopwords:
-            russian_stopwords.update(custom_stopwords)
+            combined_stopwords.update(custom_stopwords)
 
         words = text.split()
-        filtered_words = [word for word in words if word not in russian_stopwords and len(word) > 2]
+        filtered_words = [word for word in words if word not in combined_stopwords and len(word) > 2]
 
         return ' '.join(filtered_words)
 
